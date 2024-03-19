8e112407f45050782dd609ef6553d605ef242435
Kien Bui <kaybi9x@gmail.com>
2024-03-19 19:06:24 +0700

    test

---
+++ screens/Home.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {StyleSheet, View} from 'react-native'
+
+const Home = () => {
+  return (
+    <View>
+      Day la home page
+    </View>
+  )
+}
+
+export default Home
